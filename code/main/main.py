5bff37fa864b8ffc4970f5e2f5021d6f54ecd283
Shumarov Alexandr <ashum048@uottawa.ca>
2018-12-13 09:11:12 -0500

    Created main FIle. PLease create the folder DataSet. The generated images would go there. Not to put int on gitHUb it is added to gitIgnore

--- code/main/main.py
+++ code/main/main.py
@@ -1,23 +1,14 @@
 import importlib.util
 import os.path
+import sys
+sys.path.insert(0, '../img-generator')
+sys.path.insert(0, '../convNet')
+import generate_dataset
+import convNet
+import dataFeeder
 
-#importing convNet
-pathToConvNet = str(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))+"\\convNet\\convNet.py")
-#this is a path to the DataSet folder that is ignored but you should create one))
-pathToGenDataset =  str(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)))+"\\img-generator\\generate_dataset.py"
-pathToSaveDataset =  str(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)))+"\\DataSet\\"
 
-
-spec = importlib.util.spec_from_file_location("convNet",pathToConvNet)
-convNet = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(convNet)
-
-#spec = importlib.util.spec_from_file_location("generate_dataset",pathToGenDataset)
-#dataGen = importlib.util.module_from_spec(spec)
-#spec.loader.exec_module(dataGen)
-
-#generate_dataset()
-#convNet.LeNet()
+generate_dataset.generate_dataset("../DataSet")
 
 
 print()
