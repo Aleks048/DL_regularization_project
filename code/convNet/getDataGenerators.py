ff8e4572c27d09b8e0a6de615a6c44797d9a9a78
Shumarov Alexandr <ashum048@uottawa.ca>
2018-12-13 20:15:17 -0500

    The model fits

--- code/convNet/getDataGenerators.py
+++ code/convNet/getDataGenerators.py
@@ -1,6 +1,7 @@
 import os
 from myImage import *
 from dataFeeder import *
+from random import shuffle
 
 #returns the datGenerators for the model
 def getDataGenerators(imgDim,batch,numClasses,numOfChannels,shuffleInBatch,DataSetPath):
@@ -11,7 +12,12 @@ def getDataGenerators(imgDim,batch,numClasses,numOfChannels,shuffleInBatch,DataS
 	personNum =3
 	truckNum = 4
 	
-	myImages = []
+	myImagesPlane = []
+	myImagesDog = []
+	myImagesFrog = []
+	myImagesPerson = []
+	myImagesTruck = []
+
 	for subdir, dirs, files in os.walk("../DataSet"):
 		for file in files:
 			#print os.path.join(subdir, file)
@@ -19,32 +25,55 @@ def getDataGenerators(imgDim,batch,numClasses,numOfChannels,shuffleInBatch,DataS
 
 			if filepath.endswith(".png"):
 				if "airplane" in str(filepath):
-					myImages.append(MyImage(filepath,airplaneNum))
+					myImagesPlane.append(MyImage(filepath,airplaneNum))
 				if "dog" in str(filepath):
-					myImages.append(MyImage(filepath,dogNum))
+					myImagesDog.append(MyImage(filepath,dogNum))
 				if "frog" in str(filepath):
-					myImages.append(MyImage(filepath,frogNum))
+					myImagesFrog.append(MyImage(filepath,frogNum))
 				if "person" in str(filepath):
-					myImages.append(MyImage(filepath,personNum))
+					myImagesPerson.append(MyImage(filepath,personNum))
 				if "truck" in str(filepath):
-					myImages.append(MyImage(filepath,truckNum))
+					myImagesTruck.append(MyImage(filepath,truckNum))
 
 
 
 	partition ={"train" : [],"test":[]}
 
-	partition["train"] = [myImages[i].path for i in range(int(len(myImages)*0.8))]
+	partition["train"] = [myImagesPlane[i].path for i in range(int(len(myImagesPlane)*0.8))]+[myImagesDog[i].path for i in range(int(len(myImagesDog)*0.8))]+[myImagesFrog[i].path for i in range(int(len(myImagesFrog)*0.8))]+[myImagesPerson[i].path for i in range(int(len(myImagesPerson)*0.8))]+[myImagesTruck[i].path for i in range(int(len(myImagesTruck)*0.8))]
 
-	trainLabels = {myImages[i].path:myImages[i].classNum for i in range(int(len(myImages)*0.8))}
+	trainLabels = {myImagesPlane[i].path:myImagesPlane[i].classNum for i in range(int(len(myImagesPlane)*0.8))}
+	trDogLabels = {myImagesDog[i].path:myImagesDog[i].classNum for i in range(int(len(myImagesDog)*0.8))}
+	trFrogLabels = {myImagesFrog[i].path:myImagesFrog[i].classNum for i in range(int(len(myImagesFrog)*0.8))}
+	trPersonLabels = {myImagesPerson[i].path:myImagesPerson[i].classNum for i in range(int(len(myImagesPerson)*0.8))}
+	trTruckLabels = {myImagesTruck[i].path:myImagesTruck[i].classNum for i in range(int(len(myImagesTruck)*0.8))}
 
-	partition["test"] = [myImages[i].path for i in range(int(len(myImages)*0.8),len(myImages))]
-	testLabels = {myImages[i].path:myImages[i].classNum for i in range(int(len(myImages)*0.8),len(myImages))}
+	trainLabels = {**trainLabels,**trDogLabels}
+	trainLabels = {**trainLabels,**trFrogLabels}
+	trainLabels = {**trainLabels,**trPersonLabels}
+	trainLabels = {**trainLabels,**trTruckLabels}
 
+   
+
+	partition["test"] = [myImagesPlane[i].path for i in range(int(len(myImagesPlane)*0.8),len(myImagesPlane))]+[myImagesDog[i].path for i in range(int(len(myImagesDog)*0.8),len(myImagesDog))]+[myImagesFrog[i].path for i in range(int(len(myImagesFrog)*0.8),len(myImagesFrog))]+[myImagesPerson[i].path for i in range(int(len(myImagesPerson)*0.8),len(myImagesPerson))]+[myImagesTruck[i].path for i in range(int(len(myImagesTruck)*0.8),len(myImagesTruck))]
+	testLabels = {myImagesPlane[i].path:myImagesPlane[i].classNum for i in range(int(len(myImagesPlane)*0.8),len(myImagesPlane))}
+	testDogLabels = {myImagesDog[i].path:myImagesDog[i].classNum for i in range(int(len(myImagesDog)*0.8),len(myImagesDog))}
+	testFrogLabels = {myImagesFrog[i].path:myImagesFrog[i].classNum for i in range(int(len(myImagesFrog)*0.8),len(myImagesFrog))}
+	testPersonLabels = {myImagesPerson[i].path:myImagesPerson[i].classNum for i in range(int(len(myImagesPerson)*0.8),len(myImagesPerson))}
+	testTruckLabels = {myImagesTruck[i].path:myImagesTruck[i].classNum for i in range(int(len(myImagesTruck)*0.8),len(myImagesTruck))}
+	testLabels = {**testLabels,**testDogLabels}
+	testLabels = {**testLabels,**testFrogLabels}
+	testLabels = {**testLabels,**testPersonLabels}
+	testLabels = {**testLabels,**testTruckLabels}
+	shuffle(partition["test"])
+	shuffle(partition["train"])
+    
 	params = {'dim': (imgDim[0],imgDim[1]),
 				'batch_size': batch,
 				'n_classes': numClasses,
 				'n_channels': numOfChannels,
 				'shuffle': shuffleInBatch}
+
+
 
 	training_generator = DataGenerator(partition['train'], trainLabels, **params)         
 	test_generator = DataGenerator(partition['test'], testLabels, **params)	
